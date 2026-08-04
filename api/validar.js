f66c987a3621bb4dac145227975809a0c170eeff
wilfranfcoach-ops <wilfranf.coach@gmail.com>
2026-08-03 19:06:56 -0500

    fix validar gumroad

--- api/validar.js
+++ api/validar.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     }
 
     // 🧪 TEST (sin Gumroad todavía)
-    if (codigo === "123") {
+    if (codigo === "_R8g_E9Zue7FcaFGcklNXQ==") {
       return res.status(200).json({ ok: true });
     }
 
